3451f9203859ab4081d49de5e2e06f314ecd18a2
Lakshan Madushanka <epmadushanka@gmail.com>
2025-08-30 19:56:22 +0530

    fix(phpstan):

--- src/Exceptions/InvalidNameException.php
+++ src/Exceptions/InvalidNameException.php
@@ -2,10 +2,10 @@
 
 namespace Lakm\PersonName\Exceptions;
 
-class InvalidNameException extends \Exception
+final class InvalidNameException extends \Exception
 {
     public static function from(string $msg = 'Invalid name'): static
     {
-        return new static($msg);
+        return new self($msg);
     }
 }
