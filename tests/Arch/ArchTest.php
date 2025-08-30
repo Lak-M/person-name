b90ce6011a0b97282e263827523cf25e11b08187
Lakshan Madushanka <epmadushanka@gmail.com>
2025-08-30 17:17:51 +0530

    test: arch

--- tests/Arch/ArchTest.php
+++ tests/Arch/ArchTest.php
@@ -2,6 +2,25 @@
 
 declare(strict_types=1);
 
+use Lakm\PersonName\Contracts\AbbreviatorContract;
+use Lakm\PersonName\Contracts\NameBuilderContract;
+
+
 arch('globals')
     ->expect(['dd', 'dump', 'var_dump', 'print_r'])
     ->not->toBeUsed();
+
+arch()
+    ->expect('Lakm\PersonName\NameBuilders')
+    ->toBeClasses()
+    ->toExtend(NameBuilderContract::class);
+
+arch()->expect('Lakm\PersonName\Enums')
+    ->toBeEnums();
+
+arch()->expect('Lakm\PersonName\Exceptions')
+    ->toExtend(\Exception::class);
+
+arch()->expect('Lakm\PersonName\Abbreviator\Strategies')
+    ->toExtend(AbbreviatorContract::class);
+
